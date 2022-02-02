3ced7699ed1115b7003269c210449cc5d06937fe
Ibrahim <abram.pomposelli1@gmail.com>
2022-02-02 11:19:13 +0100

    exercice 6

--- cypress/e2e/exercice5.js
+++ cypress/e2e/exercice5.js
@@ -1,14 +1,15 @@
-// Supprimer Javascript, Python
-// Vérifier que la todo Javascript et Python n'existent plus
-
 describe("todo", () => {
   it("use todo", () => {
+    // Aller sur le site todo
     cy.visit("http://127.0.0.1:5501/todo.html");
+    // Vérifier que l'url est bien /todo.html
     cy.url().should("eq", "http://127.0.0.1:5501/todo.html");
+    // Vérifier que le titre est bien My To Do List
     cy.get("h2").should("have.text", "My To Do List");
 
+    // Supprimer toutes les todos
     cy.get("#myUL").then((element) => {
-      for (let i = 0; i < 6; i++) {
+      for (let i = 0; i < element[0].children.length; i++) {
         cy.get(element[0].children[i]).find(".close").click();
       }
     });
@@ -20,17 +21,24 @@ describe("todo", () => {
       "Manger du chocolat",
     ];
 
+    // Ajouter Javascript
+    // Ajouter Python
+    // Ajouter Faire les courses
+    // Ajouter Manger du chocolat
+
     todos.forEach((todo) => {
       cy.get("#myInput").clear().type(todo);
       cy.get("#myInput").should("have.value", todo);
       cy.get(".addBtn").click();
     });
 
+    // Cliquer sur Javascript
     cy.findByText("Javascript").click();
     cy.findByText("Javascript").should("have.class", "checked");
     cy.findByText("Javascript").click();
     cy.findByText("Javascript").should("not.have.class", "checked");
 
+    // Cliquer sur Manger du chocolat
     cy.findByText("Manger du chocolat").click();
     cy.findByText("Manger du chocolat").should("have.class", "checked");
     cy.findByText("Manger du chocolat").click();
@@ -39,13 +47,7 @@ describe("todo", () => {
     cy.findByText("Javascript").find(".close").click();
     cy.findByText("Python").find(".close").click();
 
-    // cy.contains("Javascript");
-    // cy.contains("Python").should("not.exist");
-
-    // cy.get(".checked > .close", { force: true }).click();
-    // cy.get(".checked > .close", { force: true }).click();
-    // cy.get(".checked > .close", { force: true }).click();
-    // cy.get(".checked > .close", { force: true }).click();
-    // cy.get(".checked > .close", { force: true }).click();
+    cy.contains("Javascript").should("not.be.visible");
+    cy.findByText("Python").should("not.be.visible");
   });
 });
---
+++ cypress/e2e/exercice6.js
@@ -0,0 +1,7 @@
+// Aller sur le site des checkboxes
+// Checker tous les boutons
+// Vérifier qu'ils sont bien checkés
+// Vérifier qu'ils ont bien la valeur qu'ils devraient avoir
+// Les uncheck vérifier qu'ils sont bien uncheck
+// Amuser vous à faire des tests avec les radios buttons en dessous
+// Lisez la doc de Cypress
